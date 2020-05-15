c4a8b25261bbffcaa98fd05cdea9718e844229bb
kaluskam <marysia.kaluska.00@gmail.com>
2020-05-15 11:20:43 +0200

    wykres + podział na dwa pliki

--- source.R
+++ source.R
@@ -1,36 +1,4 @@
-## wczytanie danych
-library(readr)
-library(data.table)
 
-january <- data.table(read_csv("201901-citibike-tripdata.csv.zip"))
-february <- data.table(read_csv("201902-citibike-tripdata.csv.zip"))
-march <- data.table(read_csv("201903-citibike-tripdata.csv.zip"))
-april <- data.table(read_csv("201904-citibike-tripdata.csv.zip"))
-may <- data.table(read_csv("201905-citibike-tripdata.csv.zip"))
-june <- data.table(read_csv("201906-citibike-tripdata.csv.zip"))
-july <- data.table(read_csv("201907-citibike-tripdata.csv.zip"))
-august <- data.table(read_csv("201908-citibike-tripdata.csv.zip"))
-september <- data.table(read_csv("201909-citibike-tripdata.csv.zip"))
-october <- data.table(read_csv("201910-citibike-tripdata.csv.zip"))
-november <- data.table(read_csv("201911-citibike-tripdata.csv.zip"))
-december <- data.table(read_csv("201912-citibike-tripdata.csv.zip"))
-## mniejsze dane do pracy, żeby szybciej działało potem usuniemy
-
-january <- january[sample(nrow(january), 10000), ]
-february <- february[sample(nrow(february), 10000), ]
-march <- march[sample(nrow(march), 10000), ]
-april <- april[sample(nrow(april), 10000), ]
-may <- may[sample(nrow(may), 10000), ]
-june <- june[sample(nrow(june), 10000), ]
-july <- july[sample(nrow(july), 10000), ]
-august <- august[sample(nrow(august), 10000), ]
-september <- september[sample(nrow(september), 10000), ]
-october <- october[sample(nrow(october), 10000), ]
-november <- november[sample(nrow(november), 10000), ]
-december <- december[sample(nrow(december), 10000), ]
-
-## lista z bazami danych
-months <- list(january, february, march, april, may, june, july, august, september, october, november, december)
 
 ## baza stacji
 raw_stations <- data.table(january[, c("end station id", "end station name", "end station latitude", "end station longitude")])
@@ -63,9 +31,10 @@ busy_stations <- busy_stations[, .N, by="id"]
 busy_stations <- stations[busy_stations]
 busy_stations <- busy_stations[order(N, decreasing = TRUE)]
 range <- busy_stations$N[1]
-image(z = z <- cos(r^2)*exp(-r/6), 
-      col  = colorRampPalette(c("white", "red"))(64))
-plot(busy_stations$longitude, busy_stations$latitude, colorRampPalette(c("white", "red"))(64))
+
+
+
+
 
 ## ---- najbardziej oblegane stacje patrząc po end station ----
 end_stations_indices <- data.table(january[,"end station id"])
